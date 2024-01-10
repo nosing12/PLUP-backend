fda0bd26439c829cc0b8e550d27e9c71515f38be
seunghun <nosing12@naver.com>
2024-01-10 19:59:03 +0900

    4.3 seeProfile

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -2,26 +2,41 @@ import client from "../client";
 import bcrypt from "bcrypt";
 
 export default {
+  Query: {
+    seeProfile: (_, { username }) =>
+      client.user.findUnique({
+        where: {
+          username,
+        },
+      }),
+  },
   Mutation: {
     createAccount: async (
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [{ username }, { email }],
-        },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
