26158604ddf22d6dd62832f46c99d4736e75fe14
seunghun <nosing12@naver.com>
2024-01-11 10:01:16 +0900

    4.8 authentication part One

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,40 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword, token }
+    ) => {
+      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassword && { password: uglyPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile",
+        };
+      }
+    },
   },
 };
