7c1ce274e3db9edbd7c5db9756fbba5b2adfbe2b
seunghun <nosing12@naver.com>
2024-01-11 10:11:18 +0900

    4.9 authentication part Two

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import schema from "./schema";
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzA0OTM0MDA2fQ.bWJun0zueN09nyrvRTGz5OWdPUhFavm1ribrth6BHCU",
+  },
 });
 
 const PORT = process.env.PORT;
